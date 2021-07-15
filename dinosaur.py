f3f80c7defb13c5ea9337cd5efc82c6a09ab2472
Steve Clark <steve.a.clark48@gmail.com>
2021-07-15 18:43:05 -0500

    Create dinosaur.py
    Set up dinosaurs.py for name, health, and attack power.

---
+++ dinosaur.py
@@ -0,0 +1,10 @@
+class Dinosaur:
+    
+    def __init__(self, name, health, attackpower):
+        self.name = name[" "]
+        self.health = health (int)
+        self.attack_power = attackpower(int)
+        return self.herd
+    
+    def attack(self, robot):
+        self.attack_power -= 10
