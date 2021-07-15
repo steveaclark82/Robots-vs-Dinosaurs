553cafa03ef004b20f1b97b4397c3a8516aae08f
Steve Clark <steve.a.clark48@gmail.com>
2021-07-15 18:45:24 -0500

    Create robot.py
    Set up for name, health, and attack power of robots.

---
+++ robot.py
@@ -0,0 +1,12 @@
+class Robot:
+    
+    def __init__(self, name, health, weapon):
+        self.name = name[" "]
+        self.health = health ()
+        self.weapon = weapon
+        return self.fleet
+    
+    def attack(self, dinosaur):
+        self.weapon -= 15
+        
+        
