5994648220c6af75183f1446878cfe69ecd532d4
Steve Clark <steve.a.clark48@gmail.com>
2021-07-15 18:42:22 -0500

    Create battelfield.py
    Wrote a battlefield class for robots vs dinosaurs from uml.

---
+++ battelfield.py
@@ -0,0 +1,36 @@
+class Battelfield:
+    
+    def __init__(self, fleet, herd):
+        self.fleet = fleet
+        self.herd = herd
+        
+    def getfleet(self):
+        return self.fleet
+    
+    def getherd(self):
+        return self.herd
+    
+    def getrun_game(self):
+        return self.run_game
+    
+    def getdisplay_welcome(self):
+        return self.display_welcome
+    
+    def getbattle(self):
+        return self.Battle
+    
+    def getdino_turn(self, dinosaur):
+        return self.dino_turn
+    
+    def getrobo_turn(self, robot):
+        return self.robo_turn
+    
+    def getshow_dino_opponent_options(self):
+        return self.show_dino_opponent_options
+    
+    def getshow_robo_opponent_option(self):
+        return self.show_robo_opponent_options
+    
+    def getdiplay_winners(self):
+        return self.display_winners
+        
