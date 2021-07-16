c98e4604c52739e73253e1649fb2e610d923916e
Steve Clark <steve.a.clark48@gmail.com>
2021-07-15 20:19:35 -0500

    Update dinosaur.py
    updated the attack method for dinosaur

--- dinosaur.py
+++ dinosaur.py
@@ -7,4 +7,15 @@ class Dinosaur:
         return self.herd
     
     def attack(self, robot):
-        self.attack_power -= 10
+        print("A {0, name} appears...".format(robot))
+        while self.health > 0 and robot.health > 0:
+            self.attack(robot)
+            print("Health of the {0, name} is now {0, health}." .format(robot))
+            if robot.health <= 0:
+                break
+            robot.attack(self)
+            print("Health is now {0, health}.".format(self))
+        if self.health > 0:
+            print("You killed the {0, name}.".format(robot))
+        elif robot.health > 0:
+            print("The {0, name} killed you.".format(robot))
