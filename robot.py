1ccd88c9488add29249cd3b6b888aa35801261e7
Steve Clark <steve.a.clark48@gmail.com>
2021-07-15 20:19:54 -0500

    Update robot.py
    updated attack method for robot

--- robot.py
+++ robot.py
@@ -7,6 +7,18 @@ class Robot:
         return self.fleet
     
     def attack(self, dinosaur):
-        self.weapon -= 15
+        def attack(self, dinosaur):
+            print("A {0, name} appears...".format(dinosaur))
+        while self.health > 0 and dinosaur.health > 0:
+            self.attack(dinosaur)
+            print("Health of the {0, name} is now {0, health}." .format(dinosaur))
+            if dinosaur.health <= 0:
+                break
+            dinosaur.attack(self)
+            print("Health is now {0, health}.".format(self))
+        if self.health > 0:
+            print("You killed the {0, name}.".format(dinosaur))
+        elif dinosaur.health > 0:
+            print("The {0, name} killed you.".format(dinosaur))
         
         
